1f5425b3abb8a581476aa9c72b38cd3deac32053
PK9811-hub <p.kyriazi@athenarc.gr>
2026-03-27 17:12:20 +0200

    Corrected data, added data loader functions in .py file and added a test_matching .ipynb file in notebooks folder to run initial matching script

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -6,6 +6,8 @@ import argparse
 from pathlib import Path
 import ast
 import random
+import logging
+logger = logging.getLogger(__name__)
 from datasets import Dataset, Image, Features, Sequence
 from huggingface_hub import create_repo, repo_exists, HfApi
 from dotenv import load_dotenv, find_dotenv
@@ -14,6 +16,8 @@ load_dotenv(find_dotenv())
 
 # --- HELPER FUNCTIONS ---
 
+# --- DATA LOADERS ---
+#identification of year, school_type, and subject based on filepath
 def parse_path_metadata(filepath):
     path_obj = Path(filepath)
     
@@ -27,12 +31,26 @@ def parse_path_metadata(filepath):
     except (ValueError, IndexError):
         return None, None, None
 
+#parsing json files' structure
+def parse_json_questions(json_path):
+    questions_list = []
+    json_path = Path(json_path)
+    
+    if not json_path.exists():
+        logger.warning(f'Προσοχή: Το αρχείο {json_path} δε βρέθηκε.')
+        return questions_list
+    
+    with json_path.open ("r", encoding = "utf-8-sig") as f:
+        data = json.load(f)
+        return data
+
+#parsing md files' structure and creation of dict with answers: {"ID": "Απάντηση"}
 def parse_markdown_answers(md_path):
     answers_dict = {}
     md_path = Path(md_path)
     
     if not md_path.exists():
-        print(f"Προσοχή: Το αρχείο {md_path} δεν βρέθηκε.")
+        logger.warning(f"Προσοχή: Το αρχείο {md_path} δε βρέθηκε.")
         return answers_dict
         
     with md_path.open("r", encoding="utf-8-sig") as f:
@@ -59,3 +77,52 @@ def parse_markdown_answers(md_path):
                 answers_dict[q_id] = ans_text
                 
     return answers_dict
+
+#merging questions (json) with answers (md) based on ids and adding metadata
+def merge_qa_data(json_path, md_path):
+    combined_data = []
+    
+    subject, school_type, year = parse_path_metadata(json_path)
+    questions_list = parse_json_questions(json_path)
+    answers_dict = parse_markdown_answers(md_path)
+    
+    for question in questions_list:
+        current_q_id = question["id"]
+        
+        if current_q_id in answers_dict:
+            question["answer"] = answers_dict[current_q_id]
+        else:
+            question["answer"] = None
+            logger.warning(f"Λείπει η απάντηση για το {current_q_id} στο μάθημα {subject} ({year})")
+        
+        question["subject"] = subject
+        question["year"] = year
+        question["school_type"] = school_type
+        
+        combined_data.append(question)
+    
+    return combined_data
+
+#folder_scanning and file_pairing returning a list of questions with their answers
+def get_file_pairs(data_dir):
+    data_dir = Path(data_dir)
+    pairs = []
+    
+    for json_path in data_dir.rglob("*.json"):
+        json_filename = json_path.name
+        md_filename = json_filename.replace("them_", "apant_").replace(".json", ".md")
+        
+        md_path = json_path.parent / md_filename
+        
+        if md_path.exists():
+            pairs.append({"json": json_path, "md": md_path})
+        else:
+            logger.warning(f"Βρέθηκε το {json_filename} χωρίς απάντηση.")
+            
+    return pairs
+
+# --- DATA TRANSFORMERS ---        
+
+
+
+# --- DATA CONSOLIDATION ---
