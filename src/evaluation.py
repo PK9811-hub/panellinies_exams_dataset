a0f83a234af91b967a9569790929282eaabe3fee
PK9811-hub <p.kyriazi@athenarc.gr>
2026-04-16 11:29:45 +0300

    Added evaluation.py file

---
+++ src/evaluation.py
@@ -0,0 +1,16 @@
+#functions to be added from protipa exams dataset
+
+#def filter_dataset()
+
+#def load_panellinies_dataset()
+
+#def process_matching_row()
+
+#def apply_matching_processing
+
+#def clean_dataset_paths()
+
+#def process_results_open() ?
+#def process_results_bypass() ?
+
+#def run_evaluation()
